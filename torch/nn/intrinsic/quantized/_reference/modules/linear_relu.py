63b693773158f4e1a969906612dbee13742bb681
Jerry Zhang <jerryzh168@gmail.com>
2021-08-25 17:55:56 -0700

    [quant][graphmode][fx] Add reference quantized linear module
    Summary:
    Added reference quantized linear module for the custom backend flow, the reference quantized module will
    have the following code:
    ```
            w(float) -- quant - dequant \
            x(float) ------------- F.linear ---
    ```
    In the full model, we will see
    ```
            w(float) -- quant - *dequant \
            x -- quant --- *dequant --  *F.linear --- *quant - dequant
    ```
    and the backend should be able to fuse the ops with `*` into a quantized linear
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx.test_conv_linear_reference
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: e114f95685921f2e5a5d9a3b2c7a815f0ace6cbc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63627

--- torch/nn/intrinsic/quantized/_reference/modules/linear_relu.py
+++ torch/nn/intrinsic/quantized/_reference/modules/linear_relu.py
@@ -2,26 +2,23 @@ import torch
 import torch.nn.intrinsic as nni
 import torch.nn.quantized._reference as nnqr
 import torch.nn.functional as F
+from typing import Optional, Dict, Any
 
 class LinearReLU(nnqr.Linear):
     _FLOAT_MODULE = nni.LinearReLU
 
     def __init__(
             self,
-            in_features,
-            out_features,
-            bias=True,
-            dtype=torch.qint8):
-        super().__init__(in_features, out_features, bias, dtype)
+            in_features: int,
+            out_features: int,
+            bias: bool = True,
+            weight_qparams: Optional[Dict[str, Any]] = None):
+        super().__init__(in_features, out_features, bias, weight_qparams)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x_dequant = x.dequantize()
-        weight_dequant = self._qweight.dequantize()
-        float_result = F.linear(x_dequant, weight_dequant, self._bias)
-        float_result = F.relu(float_result, inplace=True)
-        # NEEDFIX: we don't have dtype in the Linear module APIs right now!
-        result = torch.quantize_per_tensor(
-            float_result, self.scale, self.zero_point, torch.quint8)
+        weight_dequant = self.get_weight()
+        result = F.linear(x, weight_dequant, self.bias)
+        result = F.relu(result, inplace=True)
         return result
 
     def _get_name(self):
