9ff00555b49bdd2f283bc27498b8023b72d92ed5
Will Constable <whc@fb.com>
2021-08-25 18:59:10 -0700

    WIP handling input (tensor) values in IR node

--- tools/codegen/dest/lazy_ir.py
+++ tools/codegen/dest/lazy_ir.py
@@ -6,13 +6,14 @@ import textwrap
 
 from tools.codegen.context import method_with_native_function, native_function_manager
 from tools.codegen.utils import Target, mapMaybe
-from tools.codegen.model import (DispatchKey, NativeFunction,
+from tools.codegen.model import (BaseType, OptionalType, DispatchKey, NativeFunction,
                                  NativeFunctionsGroup, SchemaKind,
                                  TensorOptionsArguments,
                                  DeviceCheckType, Argument, assert_never,
                                  is_cuda_dispatch_key, BackendIndex,
                                  gets_generated_out_inplace_wrapper)
-from tools.codegen.api.types import (BaseCType, Binding, ConstRefCType,
+from tools.codegen.api.types import (BaseTy, BaseCppType, BaseCType, OptionalCType,
+                                     Binding, ConstRefCType,
                                      CppSignature, CppSignatureGroup,
                                      Expr, MutRefCType, kernel_signature,
                                      NativeSignature, tensorT, NamedCType,
@@ -46,16 +47,64 @@ class LazyIR:
 
     @method_with_native_function
     def __call__(self, f: Union[NativeFunctionsGroup, NativeFunction]) -> List[str]:
-        if f.func.name in self.backend_index.index:
+        if isinstance(f, NativeFunctionsGroup):
+            func = f.functional.func
+        else:
+            func = f.func
+        # print(func.name)
+        if func.name in self.backend_index.index:
             return self.gen(f)
         else:
             return []
 
     def gen(self, f: Union[NativeFunctionsGroup, NativeFunction]) -> List[str]:
         # for now, we just want one IR class decl and soon after also the method defs
-        return ["// blah"]
+        # and we use the functional version not out/inplace.
+        func = f.functional.func if isinstance(f, NativeFunctionsGroup) else f.func
+        class_name = str(func.name).lower().capitalize()
 
-# TODO(whc) do we stick with the GenericOp concept or just make a class per IR?
-#   NodePtr {name}(const Value& input) {                       \
-    # return GenericOp(OpKind({sym}), {input}, input.shape()); \
-  
+        """IR Value inputs are IR values that stand in for Tensors in the function schema.
+
+           Lets assume for now that we just want to pass through optional Tensors as
+           optional Values and let the lowerings handle the rest.
+        """
+        valueT = BaseCppType('ir', 'Value')
+
+        def value_inputs():
+            """This just filters the function args to find the Tensors (incl optionals),
+            and makes them into Value types
+            """
+            ret = []
+            for t in func.arguments.post_self_positional:
+                if t.is_write:
+                    assert False, "Do we expect mutable tensors in lazytensor bindings?"
+                else:
+                    if isinstance(t.type, BaseType) and t.type.name == BaseTy.Tensor:
+                        ret.append(NamedCType(t.name, ConstRefCType(BaseCType(valueT))))
+                    elif isinstance(t.type, OptionalType) and str(t.type.elem) == 'Tensor':
+                        ret.append(NamedCType(t.name, ConstRefCType(OptionalCType(BaseCType(valueT)))))
+                    else:
+                        # assert False, f"didn't get to lists or others yet, {t}"
+                        pass
+            return ret
+
+        return [f"""\
+class {class_name} : public LazyNodeBase {{
+public:
+{class_name}({", ".join([f"{i.cpp_type()} {i.name}" for i in value_inputs()])})
+    : LazyNodeBase(ir::OpKind(at::aten::{func.name.name})),
+            {{{", ".join([f"{i.name}" for i in value_inputs()])}}},
+            /*num_outputs=*/{len(func.returns)},
+            lazy_tensors::util::MHash(/*TODO*/),
+    dtype_(dtype)
+{{}}
+
+std::string ToString() const override;
+
+NodePtr Clone(OpList operands) const override;
+
+private:
+std::vector<c10::Optional<ir::Value>> inputs;
+
+}}
+""", ]
